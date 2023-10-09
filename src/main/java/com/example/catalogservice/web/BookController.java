683d165a0e8a895807c70e75b4eba5ef84928dfe
cabbarove <Elshad.Cabbarov@kapitalbank.az>
2023-10-09 10:32:07 +0400

    added data layer, db migration

--- src/main/java/com/example/catalogservice/web/BookController.java
+++ src/main/java/com/example/catalogservice/web/BookController.java
@@ -2,9 +2,10 @@ package com.example.catalogservice.web;
 
 import com.example.catalogservice.domain.Book;
 import com.example.catalogservice.domain.BookService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("books")
