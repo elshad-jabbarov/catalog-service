df13ed2b9294346679ee8bd84926141e5ed77762
cabbarove <Elshad.Cabbarov@kapitalbank.az>
2024-01-17 12:15:51 +0400

    added docker compose

--- src/main/java/com/example/catalogservice/domain/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.catalogservice.domain;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String isbn) {
-        super("The book with ISBN " + isbn + " was not found.");
-    }
-}
